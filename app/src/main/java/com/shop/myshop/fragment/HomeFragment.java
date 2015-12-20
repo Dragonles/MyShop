3500e2d46805a6fe9b8d890ad11f7ec48f026855
Dragonles <981506405@qq.com>
2015-12-20 20:45:57 +0800

    add login regist from lolong

--- app/src/main/java/com/shop/myshop/fragment/HomeFragment.java
+++ app/src/main/java/com/shop/myshop/fragment/HomeFragment.java
@@ -9,8 +9,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import com.shop.myshop.myshop.AddressActivtiy;
+import com.shop.myshop.myshop.CaptureActivity;
 import com.shop.myshop.myshop.CareActivity;
 import com.shop.myshop.myshop.LoginActivity;
 import com.shop.myshop.myshop.R;
@@ -21,7 +23,7 @@ public class HomeFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    ImageButton mclass;
+    ImageButton mclass,mqrcode;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -44,6 +46,14 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_home,container,false);
+        mqrcode=(ImageButton)v.findViewById(R.id.qrcode);
+        mqrcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(getActivity(), CaptureActivity.class);
+                startActivity(intent);
+            }
+        });
         mclass=(ImageButton)v.findViewById(R.id.allclass);
         mclass.setOnClickListener(new View.OnClickListener() {
             @Override
